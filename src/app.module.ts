f919ac45a5d1f3a348fdcf4ecca8f4e0ec1a05b1
ZackFox <firstthinker1989@gmail.com>
2019-05-19 14:57:19 +0500

    Error filter

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,25 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Connection } from 'typeorm';
+import * as path from 'path';
+
+import { ResumeController } from './resume/resume.controller';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      url: process.env.DATABASE_URL,
+      synchronize: false,
+      entities: [path.join(__dirname, '../', '/src/**/*.entity.ts')],
+      // migrations: [path.join(__dirname, "src/migrations/*.ts")],
+      logging: true,
+      ssl: true,
+    }),
+  ],
+  controllers: [ResumeController],
+  providers: [],
 })
-export class AppModule {}
+export class AppModule {
+  constructor(private readonly connection: Connection) {}
+}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
+import 'dotenv/config';
+
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ErrorFilter } from './app.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.setGlobalPrefix('api/v1');
+  app.useGlobalFilters(new ErrorFilter());
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
