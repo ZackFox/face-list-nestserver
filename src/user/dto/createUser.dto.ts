00349dcf32a62291371acc6745309684814f8a3b
ZackFox <firstthinker1989@gmail.com>
2019-06-12 13:12:26 +0500

    add swagger gecorators

--- src/user/dto/createUser.dto.ts
+++ src/user/dto/createUser.dto.ts
@@ -5,30 +5,38 @@ import {
   MinLength,
   IsEnum,
 } from 'class-validator';
+import { ApiModelProperty } from '@nestjs/swagger';
 
 import { Genders } from '../../shared/gender.enum';
 
 export class CreateUserDto {
   @IsNotEmpty()
+  @ApiModelProperty()
   readonly firstname: string;
 
   @IsNotEmpty()
+  @ApiModelProperty()
   readonly lastname: string;
 
   @IsNumber()
+  @ApiModelProperty()
   readonly age: number;
 
   @IsNotEmpty()
+  @ApiModelProperty()
   readonly city: string;
 
   @IsEnum(Genders, { message: 'Значение может быть "мужчина" или "женщина" ' })
+  @ApiModelProperty({ enum: ['мужчина', 'женщина'] })
   readonly gender: Genders;
 
   @IsEmail()
   @IsNotEmpty()
+  @ApiModelProperty()
   readonly email: string;
 
   @MinLength(5)
   @IsNotEmpty()
+  @ApiModelProperty()
   readonly password: string;
 }
